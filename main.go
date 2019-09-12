1627beefad2d4b8408e7d9a2d33cc87f4fb2dab4
goldcap <amadsen@operislabs.com>
2019-09-12 14:51:31 -0400

    master: Updated Release Version

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 // HandlerFunc1 handles basic calls
 func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\nRelease 1.012 - %s", os.Getenv("STRICTLY_CROSSOVER"))
+	fmt.Fprintf(w, "\nRelease 1.013 - %s", os.Getenv("STRICTLY_CROSSOVER"))
 }
 
 func main() {
